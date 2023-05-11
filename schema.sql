4ec9e5b06065add701b151fb40048cf7a9d1d075
Daniil Ivanov <iva.dan.den@gmail.com>
2023-05-11 20:57:43 +0300

    HW 7 - Daniil Ivanov

---
+++ schema.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE IF NOT EXISTS `hillel_php_basic_daniil_ivanov`;
+USE `hillel_php_basic_daniil_ivanov`;
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `projects` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `user_id` int(11) NOT NULL,
+    `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `p_user_id` (`user_id`),
+    CONSTRAINT `p_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `tasks` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `project_id` int(11) NOT NULL,
+    `user_id` int(11) NOT NULL,
+    `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `duration` date NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `t_project_id` (`project_id`),
+    KEY `t_user_id` (`user_id`),
+    CONSTRAINT `t_project_id` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `t_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
