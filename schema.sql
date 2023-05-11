e63c75235d8e6869a210059161ae96306d04e7e8
Daniil Ivanov <iva.dan.den@gmail.com>
2023-05-11 21:19:05 +0300

    HW 7 - Daniil Ivanov #2

--- schema.sql
+++ schema.sql
@@ -24,10 +24,12 @@ CREATE TABLE IF NOT EXISTS `tasks` (
     `user_id` int(11) NOT NULL,
     `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
     `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-    `duration` date NULL DEFAULT NULL,
+    `deadline` date DEFAULT NULL,
+    `status` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `files` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
     PRIMARY KEY (`id`),
     KEY `t_project_id` (`project_id`),
     KEY `t_user_id` (`user_id`),
     CONSTRAINT `t_project_id` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT `t_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
